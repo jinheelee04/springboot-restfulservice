b53161f15c714489358e5a5e1fd87e3c75dfed2c
jinheelee04 <2pro.jlee@gmail.com>
2024-01-24 21:43:36 +0900

    RESTful Service

---
+++ my-restful-service/src/main/java/com/jproject/myrestfulservice/MyRestfulServiceApplication.java
@@ -0,0 +1,18 @@
+package com.jproject.myrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MyRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = SpringApplication.run(MyRestfulServiceApplication.class, args);
+//		String[] allBeanNames = ac.getBeanDefinitionNames();
+//		for (String beanName : allBeanNames) {
+//			System.out.println(beanName);
+//		}
+	}
+
+}
