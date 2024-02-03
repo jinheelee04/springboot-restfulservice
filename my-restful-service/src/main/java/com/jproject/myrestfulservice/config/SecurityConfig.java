b4d9570dea834232b6bb64db9f9e965623eea3ef
jinheelee04 <2pro.jlee@gmail.com>
2024-02-03 16:14:34 +0900

    Spring Data JPA를 이용한 초기 데이터 생성

--- my-restful-service/src/main/java/com/jproject/myrestfulservice/config/SecurityConfig.java
+++ my-restful-service/src/main/java/com/jproject/myrestfulservice/config/SecurityConfig.java
@@ -2,28 +2,36 @@ package com.jproject.myrestfulservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
-//    @Bean
-//    UserDetailsService userDetailsService(){
-//        // 메모리 객체를 이용해 storage로 사용
-//        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-//        UserDetails newUser = User.withUsername("user")
-//                      .password(passwordEncoder().encode("passw0rd"))
-//                      .authorities("read")
-//                      .build();
-//        userDetailsManager.createUser(newUser);
-//        return userDetailsManager;
-//    }
-//
-//    @Bean
-//    BCryptPasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
+    @Bean
+    UserDetailsService userDetailsService(){
+        // 메모리 객체를 이용해 storage로 사용
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails newUser = User.withUsername("user")
+                      .password(passwordEncoder().encode("passw0rd"))
+                      .authorities("read")
+                      .build();
+        userDetailsManager.createUser(newUser);
+        return userDetailsManager;
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
 }
---
+++ my-restful-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into users(id, join_date, name, password, ssn) values(90001, now(), 'User1', 'test111', '940101-1111111')
+insert into users(id, join_date, name, password, ssn) values(90002, now(), 'User2', 'test222', '940202-2222222')
+insert into users(id, join_date, name, password, ssn) values(90003, now(), 'User3', 'test333', '940303-3333333')
