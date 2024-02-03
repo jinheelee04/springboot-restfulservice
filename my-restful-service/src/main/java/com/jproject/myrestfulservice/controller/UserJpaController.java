6768531f3ccc07076364c8b1b016f9df0f5ce73b
jinheelee04 <2pro.jlee@gmail.com>
2024-02-03 18:44:43 +0900

    JPA를 이용한 새 게시물 추가

--- my-restful-service/src/main/java/com/jproject/myrestfulservice/controller/UserJpaController.java
+++ my-restful-service/src/main/java/com/jproject/myrestfulservice/controller/UserJpaController.java
@@ -4,9 +4,9 @@ import com.jproject.myrestfulservice.bean.Post;
 import com.jproject.myrestfulservice.bean.ResponseData;
 import com.jproject.myrestfulservice.bean.User;
 import com.jproject.myrestfulservice.exception.UserNotFoundException;
+import com.jproject.myrestfulservice.repository.PostRepository;
 import com.jproject.myrestfulservice.repository.UserRepository;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -26,9 +24,11 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping("/jpa")
 public class UserJpaController {
     private UserRepository userRepository;
+    private PostRepository postRepository;
 
-    public UserJpaController(UserRepository userRepository) {
+    public UserJpaController(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
 
     @GetMapping("/users")
@@ -76,5 +76,21 @@ public class UserJpaController {
             throw new UserNotFoundException("id-"+id);
         }
         return user.get().getPosts();
+    }
+    
+    @PostMapping("/users/{id}/posts")
+    public ResponseEntity createPost(@PathVariable int id, @RequestBody Post post){
+        Optional<User> userOptional = userRepository.findById(id);
+        if(!userOptional.isPresent()){
+            throw new UserNotFoundException("id="+id);
+        }
+        User user = userOptional.get();
+        post.setUser(user); // 사용자ID 연결
+        postRepository.save(post);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(post.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
---
+++ my-restful-service/src/main/java/com/jproject/myrestfulservice/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.jproject.myrestfulservice.repository;
+
+import com.jproject.myrestfulservice.bean.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+}
